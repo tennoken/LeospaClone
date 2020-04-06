c4bb955b5d9c818e13111663d5a6fb4816fe9fb4
Kzero <tennoken@naver.com>
2020-04-06 12:37:24 +0900

    create Testimonial section

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const closebtn = document.querySelector(".close");
 const videoBtn = document.querySelector(".video-btn");
 const video = document.querySelector(".video");
 
-videoBtn.onclick = function() {
+videoBtn.onclick = function () {
   modal.style.display = "block";
 };
 
-close.onclick = function() {
+closebtn.onclick = function () {
   modal.style.display = "none";
   video.src = video.src;
 };
-
-const swiper = new Swiper(".swiper-container", {
-  slidesPerView: 4,
-  centeredSlides: false,
-  spaceBetween: 10,
-  grabCursor: true
-});
