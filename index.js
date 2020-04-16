7fb430b0ba7fb420346480c879f4f13c2d7c3533
Kzero <tennoken@naver.com>
2020-04-16 12:25:42 +0900

    update media 768px

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const modal = document.querySelector(".modal");
 const closebtn = document.querySelector(".close");
 const videoBtn = document.querySelector(".video-btn");
 const video = document.querySelector(".video");
+const navicon = document.querySelector(".navicon");
+const navMenu = document.querySelector(".nav-menu");
 
-videoBtn.onclick = function () {
+videoBtn.addEventListener("click", function () {
   modal.style.display = "block";
-};
+});
 
-closebtn.onclick = function () {
+closebtn.addEventListener("click", function () {
   modal.style.display = "none";
   video.src = video.src;
-};
+});
+
+navicon.addEventListener("click", function () {
+  navMenu.classList.toggle("open");
+});
